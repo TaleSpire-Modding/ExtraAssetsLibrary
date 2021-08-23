57b33a585b9a682dd76582d05f43f0e1431e05a8
brajm008 <ujkkju117@outlook.com>
2021-08-23 19:14:46 +0930

    cleanup couple imports.

--- ExtraAssetsLibrary/DTO/Asset.cs
+++ ExtraAssetsLibrary/DTO/Asset.cs
@@ -39,7 +39,7 @@ namespace ExtraAssetsLibrary.DTO
             // Use input string to calculate MD5 hash
             using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
             {
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
                 byte[] hashBytes = md5.ComputeHash(inputBytes);
 
                 // Convert the byte array to hexadecimal string
