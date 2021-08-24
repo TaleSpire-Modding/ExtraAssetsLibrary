db350cbac6a8412fa54bd0075d1c2929a535dbdc
brajm008 <ujkkju117@outlook.com>
2021-08-24 19:14:43 +0930

    bump version

--- ExtraAssetsLibrary/ExtraAssetPlugin.cs
+++ ExtraAssetsLibrary/ExtraAssetPlugin.cs
@@ -13,7 +13,7 @@ namespace ExtraAssetsLibrary
     {
         // constants
         public const string Guid = "org.TMC.plugins.ExtraAssetLib";
-        public const string Version = "1.0.0.0";
+        public const string Version = "1.0.1.0";
         private const string Name = "TMCs' Extra Asset Library";
 
         public static void DoPatching()
